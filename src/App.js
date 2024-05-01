f34cfd2c53a3d1c3ff9b56f9deddd73f47add0f4
Amariah <amariah.abish@gmail.com>
2024-05-01 09:59:34 +0300

    full test

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
-function App(){
+const App = () => {
     const [users, setUsers] = useState([]);
 
-    useEffect(() =>{
-        fetch("http://localhost:4000/users")
-        .then(r => r.json())
-        .then(data => setUsers(data))
-        .catch(error => console.error(error));
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await fetch("http://localhost:4000/users");
+                if (!response.ok) {
+                    throw new Error("Failed to fetch users");
+                }
+                const data = await response.json();
+                setUsers(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchUsers();
     }, []);
 
-    return(
+    return (
         <>
             <header>
                 <NavBar />
             </header>
-            <Outlet context={users}/>
+            <Outlet context={users} />
         </>
     );
 };
